e78839c11621af34f6d80351610d43b462a6978a
Michael Antoun <antounmichael@yahoo.com>
2023-11-04 22:44:02 -0700

    refactored functions, added class added recursive text splitter

--- testing.py
+++ testing.py
@@ -4,15 +4,14 @@ import os
 import openai
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
-
+from openai.error import Timeout
 
 import re
 import requests
 import xml.etree.ElementTree as ET
-import timeout_decorator
 
 
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -26,6 +25,36 @@ from langchain.prompts import PromptTemplate
 
 openai_api_key = "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"
 os.environ['OPENAI_API_KEY'] = openai_api_key
+
+class LlmHandler:
+
+    def __init__(self):
+        self.text_splitter = RecursiveCharacterTextSplitter(separators = ["\n\n", '\n', '.'], chunk_size=1000, chunk_overlap=0)
+        self.llm=ChatOpenAI(
+                openai_api_key=openai_api_key,
+                temperature=0, model_name="gpt-3.5-turbo", max_tokens=500, request_timeout = 15, max_retries=1
+            )
+        
+        
+    def evaluate_queries(self, embedding, queries):
+        chatbot = RetrievalQA.from_chain_type(
+            llm=self.llm, 
+            chain_type="stuff", 
+            retriever=embedding.as_retriever(search_type="similarity", search_kwargs={"k":1})
+        )
+        
+        template = """ {query}? """
+        response = []
+        for q in queries:
+            prompt = PromptTemplate(
+                input_variables=["query"],
+                template=template,
+            )
+
+            response.append(chatbot.run(
+                prompt.format(query=q)
+            ))
+        return response
 
 
 def get_pmc_paper(pmcid):
@@ -58,77 +87,24 @@ def embed_article(pmcid):
     with open(txtfilepath, 'w', encoding="utf-8") as file:
         file.write(methods_text)
 
-    print('text copied')
-
     loader = TextLoader(txtfilepath, autodetect_encoding=True)
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(separator = ".", chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(separators = ["\n\n", '\n', '.'], chunk_size=1000, chunk_overlap=0)
+        
     docs = text_splitter.split_documents(documents)
 
     faissIndex = FAISS.from_documents(docs, OpenAIEmbeddings())
     faissIndex.save_local(filepath)
     return FAISS.load_local(filepath, OpenAIEmbeddings())
 
-
-def run_test(pmcid: str, queries: [str]):
-    ## Write to file
-    # pmcid = 'PMC9935389' 
-    #pmcid = 'PMC10081221'
-    text = get_pmc_paper(pmcid)
-    methods_text = extract_methods_from_pmc_paper(text)
-    with open('input_file.txt', 'w') as file:
-        file.write(methods_text)
-
-
-    loader = TextLoader("./input_file.txt")
-    documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=50, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
-
-    faissIndex = FAISS.from_documents(docs, OpenAIEmbeddings())
-    current_document = "input_doc"
-    faissIndex.save_local(current_document)
-    #embeddings have been saved
-
-    chatbot = RetrievalQA.from_chain_type(
-        llm=ChatOpenAI(
-            openai_api_key=openai_api_key,
-            temperature=0, model_name="gpt-3.5-turbo", max_tokens=50, timeout = 9, max_retries=0
-        ), 
-        chain_type="stuff", 
-        retriever=FAISS.load_local(current_document, OpenAIEmbeddings())
-            .as_retriever(search_type="similarity", search_kwargs={"k":1})
-    )
-
-    for q in queries:
-
-        template = """ {query}? """
-
-        prompt = PromptTemplate(
-            input_variables=["query"],
-            template=template,
-        )
-
-        print(chatbot.run(
-            prompt.format(query="Does the paper report a new structure of a biomolecule or biomolecular complex modeled using experimental data")
-        ))
-
-    print('finished queries')
-
-
-    # function that takes context, prompts, and returns answers 
-
-    # comparison of answers to known answers
-
-    # 
-
-def fetch_embedding(pmcid):
+def fetch_embedding(pmcid, bypass_cache=False):
+    if bypass_cache:
+        return embed_article(pmcid)
     try:
         filepath = f'./data/{pmcid}'
         return FAISS.load_local(filepath, OpenAIEmbeddings())
-    except e:
-        print(e)
-        print('failure')
+    except Exception as e:
+        print("failed to load cached embedding, generating embedding")
         return embed_article(pmcid)
 
 
@@ -142,51 +118,22 @@ def compare_against_known():
         #run_test(pmc, )
         pass
 
-def embed_all():
+queries = ["Are there experimental techniques beyond using Cryo-Em incorporated in the paper? Answer with Yes or No followed by the experimental technique."]
+
+def run_all_tests(handler):
     pmc_ids = ['PMC8536336', 'PMC7417587', 'PMC5957504', 'PMC7492086', 'PMC9293004', 
     'PMC7854634', 'PMC5648754', 'PMC8022279', 'PMC8655018', 'PMC8916737']
     for pmcid in pmc_ids:
-        embedding = fetch_embedding(pmcid)
-        result = evaluate_query(embedding, queries)
-        print(result)
-
-
-queries = ["Are there experimental techniques beyond using Cryo-Em incorporated in the paper? Answer with Yes or No followed by the experimental technique."]
-
-def evaluate_query(embedding, queries):
-    chatbot = RetrievalQA.from_chain_type(
-        llm=ChatOpenAI(
-            openai_api_key=openai_api_key,
-            temperature=0, model_name="gpt-3.5-turbo", max_tokens=50, request_timeout = 9, max_retries=0
-        ), 
-        chain_type="stuff", 
-        retriever=embedding.as_retriever(search_type="similarity", search_kwargs={"k":1})
-
-    )
-    
-    template = """ {query}? """
-    for q in queries:
-        prompt = PromptTemplate(
-            input_variables=["query"],
-            template=template,
-        )
-
-        return(chatbot.run(
-            prompt.format(query=q)
-        ))
-
-
+        embedding = fetch_embedding(pmcid, True)
+        try:
+            result = handler.evaluate_queries(embedding, queries)
+            print(result)
+        except Timeout as e:
+            print('timeout')
 
 
     
 
+handler = LlmHandler()
+run_all_tests(handler)
 
-embed_all()
-
-def main():
-    #silly example
-    queries = ["Are there experimental techniques beyond using Cryo-Em incorporated in the paper? Answer with Yes or No followed by the experimental technique."]
-    run_test(pmcid='PMC23402394802394', queries=queries)
-
-
-print(fetch_embedding('PMC8536336'))
